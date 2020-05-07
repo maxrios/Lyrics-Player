7cfe82c2ab318c3992d5b0a598ebb4972de64b3a
Maximiliano Rios <52523737+mrios1999@users.noreply.github.com>
2020-05-07 01:45:08 -0400

    maintenance update

--- script.js
+++ script.js
@@ -138,5 +138,5 @@ function additionalVideos(data) {
   if (!initialLoad) {
     removeElement('additional-videos');
   }
-  addElement('optional-videos', 'div', 'additional-videos', '  <h6>Not the right video? Try on of these:</h6> <ol> <li> <a target=\"_blank\" href=\'https://www.youtube.com/watch?v=' + data.items[1].id.videoId + '\'>' + data.items[1].snippet.title + '</a> </li><li> <a target=\"_blank\" href=\'https://www.youtube.com/watch?v=' + data.items[2].id.videoId + '\'>' + data.items[2].snippet.title + '</a> </li><li> <a target=\"_blank\" href=\'https://www.youtube.com/watch?v=' + data.items[3].id.videoId + '\'>' + data.items[3].snippet.title + '</a> </li> </ol>')
+  addElement('optional-videos', 'div', 'additional-videos', '  <h3>Not the right video? Try on of these:</h6> <ol> <li> <a target=\"_blank\" href=\'https://www.youtube.com/watch?v=' + data.items[1].id.videoId + '\'>' + data.items[1].snippet.title + '</a> </li><li> <a target=\"_blank\" href=\'https://www.youtube.com/watch?v=' + data.items[2].id.videoId + '\'>' + data.items[2].snippet.title + '</a> </li><li> <a target=\"_blank\" href=\'https://www.youtube.com/watch?v=' + data.items[3].id.videoId + '\'>' + data.items[3].snippet.title + '</a> </li> </ol>')
 }
